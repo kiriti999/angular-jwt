c47854bc5c91dec773a1abaa27bb1e406c0b2376
Kiriti Komaragiri <Kiriti_Komaragiri@epam.com>
2020-07-18 23:47:27 +0530

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: true,
-  API_URL: 'http://localhost:4000'
+    production: true,
+    API_URL: 'ec2-18-188-66-90.us-east-2.compute.amazonaws.com'
+    // API_URL: '18.188.66.90'
 };
